3b5e434463513cebe034ac1d7ae1657a5c3f5bec
Hammad Sanaullah <hammad.sanaullah@stixor.com>
2022-12-06 02:44:27 +0500

    integrate axios graphql query

--- routes/price.js
+++ routes/price.js
@@ -4,21 +4,24 @@ const router = express.Router();
 
 router.get("/get24hourPrices", async (req, res) => {
   try {
-    axios({
+    let result = await axios({
       url: 'https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet',
       method: 'post',
+      headers: {'content-type': 'application/json'},
       data: {
-        query: `
-          tokenHourSnapshots(where: {token_: {symbol: "BTCB"}}){
-              id
-              priceNative
-              date
-            }
+        query: `{
+          tokenHourSnapshots(where: { token_: { symbol: "BTCB" } }) {
+            id
+            priceNative
+            date
+          }
+        }
           `
       }
-    }).then((result) => {
-      console.log(result.data)
-    });
+    })
+
+    console.log(result?.data?.data?.tokenHourSnapshots);
+    return res.status(200).json({message: "yayyy"});
 
   } catch (error) {
     return res.status(400).json({ message: error.message });
