f143055393093885df5fb820d9ffd54d6e823062
Hammad Sanaullah <hammad.sanaullah@stixor.com>
2022-12-06 02:08:03 +0500

    configuration complete

--- routes/price.js
+++ routes/price.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 router.get("/get24hourPrices", async (req, res) => {
   try {
+    axios({
+      url: 'https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet',
+      method: 'post',
+      data: {
+        query: `
+          tokenHourSnapshots(where: {token_: {symbol: "BTCB"}}){
+              id
+              priceNative
+              date
+            }
+          `
+      }
+    }).then((result) => {
+      console.log(result.data)
+    });
 
   } catch (error) {
     return res.status(400).json({ message: error.message });
