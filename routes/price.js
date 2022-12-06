32a06930c63266599a1935ddd86fafb2371f857c
Hammad Sanaullah <hammad.sanaullah@stixor.com>
2022-12-06 15:27:46 +0500

    add get day prices

--- routes/price.js
+++ routes/price.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 router.get("/get24hourPrices", async (req, res) => {
   try {
-    let result = await axios({
+    
+    let tokenhoursnapshotsdata = await axios({
       url: 'https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet',
       method: 'post',
-      headers: {'content-type': 'application/json'},
+      headers: {'content-type': 'application/json', "Accept-Encoding": "utf-8"},
       data: {
         query: `{
           tokenHourSnapshots(where: { token_: { symbol: "BTCB" } }) {
@@ -18,13 +19,17 @@ router.get("/get24hourPrices", async (req, res) => {
         }
           `
       }
-    })
+    });
 
-    console.log(result?.data?.data?.tokenHourSnapshots);
+    if(tokenhoursnapshotsdata.data.data.tokenHourSnapshots) {
+
+    }
+
+    console.log(tokenhoursnapshotsdata.data.data.tokenHourSnapshots);
     return res.status(200).json({message: "yayyy"});
 
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res.status(400).json({ message: error });
   }
 });
 
