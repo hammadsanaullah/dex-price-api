2a0e0cf236cc8e8abab6e1bd06c1e2ffbd65377e
Hammad Sanaullah <hammad.sanaullah@stixor.com>
2022-12-06 17:56:28 +0500

    updates

--- routes/price.js
+++ routes/price.js
@@ -2,9 +2,22 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
+
+async function generateTimeStampsForDay(previousDayTime, tokenTimeStamps) {
+
+  var timeStamps = [];
+  timeStamps[0] = previousDayTime;
+
+  for(var i = 1; i < 24; i++) {
+    timeStamps.push(timeStamps[ i - 1 ] + 3600);
+  }
+
+  return timeStamps;
+}
+
 router.get("/get24hourPrices", async (req, res) => {
   try {
-    // Date.now() + 86400000
+
     var dayStartTime = Math.round(Date.now() / 1000);
     dayStartTime = dayStartTime - 86400;
     console.log(dayStartTime);
@@ -52,6 +65,7 @@ router.get("/get24hourPrices", async (req, res) => {
       tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length != 0 &&
       tokenBhoursnapshotsdata.data.data.tokenHourSnapshots.length == 0
     ) {
+
       //if tokenhoursnapshots exist but the priceNative is 0
       if (
         tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[
@@ -59,16 +73,26 @@ router.get("/get24hourPrices", async (req, res) => {
         ].priceNative == 0
       ) {
         //have to return here that create a pair with WBNB
+        return res
+            .status(500)
+            .json({ message: "No pair created with WBNB!!!" });
+
       } else {
+
         //if price is not 0 then iterate and get all the hourly price data
-        var priceArray = [];
+        var priceAArray = [];
+        var timeStampsAArray = [];
+
         for (
           var i = 0;
           i < tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length;
           i++
         ) {
-          priceArray.push(
+          priceAArray.push(
             tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[i].priceNative
+          );
+          timeStampsAArray.push(
+            tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[i].date
           );
         }
 
@@ -90,12 +114,10 @@ router.get("/get24hourPrices", async (req, res) => {
               `,
           },
         });
-        if (tokenBData.data.data.tokenPrices.length != 0) {
-          // for(var i = 0; i < tokenData.data.data.tokenPrices.length; i++ ) {
 
-          // }
-          // console.log(tokenData.data.data.tokenPrices)
-          const lastPrice =
+        // var lastPrice;
+        if (tokenBData.data.data.tokenPrices.length != 0) {
+          var lastPrice =
             tokenBData.data.data.tokenPrices[
               tokenBData.data.data.tokenPrices.length - 1
             ].derivedNative;
@@ -105,6 +127,12 @@ router.get("/get24hourPrices", async (req, res) => {
             .status(500)
             .json({ message: "No pair created with WBNB!!!" });
         }
+/////////////////////
+        var timestamps = await generateTimeStampsForDay(dayStartTime);
+        // for(var i = 0; i < timestamps.length; i++) {
+          
+        // }
+        let constructedData = { data: }
       }
     } else if (
       tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length == 0 &&
@@ -172,40 +200,40 @@ router.get("/get24hourPrices", async (req, res) => {
       if (
         tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[
           tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length - 1
-        ].priceNative == 0 || tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[
+        ].priceNative == 0 ||
+        tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[
           tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length - 1
         ].priceNative == 0
-      ) { 
+      ) {
         //return with create a pair with WBNB
       } else {
-
         var priceAArray = [];
         for (
           var i = 0;
           i < tokenAhoursnapshotsdata.data.data.tokenHourSnapshots.length;
           i++
         ) {
-          priceArray.push(
+          priceAArray.push(
             tokenAhoursnapshotsdata.data.data.tokenHourSnapshots[i].priceNative
           );
         }
 
-        
         var priceBArray = [];
         for (
           var i = 0;
           i < tokenBhoursnapshotsdata.data.data.tokenHourSnapshots.length;
           i++
         ) {
-          priceArray.push(
+          priceBArray.push(
             tokenBhoursnapshotsdata.data.data.tokenHourSnapshots[i].priceNative
           );
         }
-      }
 
+        //return the derivedBNB
+      }
     } else {
       //else check tokenData for last price
-      let tokenData = await axios({
+      let tokenAData = await axios({
         url: "https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet",
         method: "post",
         headers: {
@@ -223,14 +251,41 @@ router.get("/get24hourPrices", async (req, res) => {
             `,
         },
       });
-      if (tokenData.data.data.tokenPrices.length != 0) {
+
+      let tokenBData = await axios({
+        url: "https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet",
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+          "Accept-Encoding": "utf-8",
+        },
+        data: {
+          query: `{
+            tokenPrices(where: {token_: {symbol: "USDT"}}) {
+              id
+              lastUsdPrice
+              derivedNative
+            }
+          }
+            `,
+        },
+      });
+      if (
+        tokenAData.data.data.tokenPrices.length != 0 ||
+        tokenBData.data.data.tokenPrices.length != 0
+      ) {
         // for(var i = 0; i < tokenData.data.data.tokenPrices.length; i++ ) {
 
         // }
         // console.log(tokenData.data.data.tokenPrices)
-        const lastPrice =
-          tokenData.data.data.tokenPrices[
-            tokenData.data.data.tokenPrices.length - 1
+        const lastPriceA =
+          tokenAData.data.data.tokenPrices[
+            tokenAData.data.data.tokenPrices.length - 1
+          ].derivedNative;
+
+        const lastPriceB =
+          tokenBData.data.data.tokenPrices[
+            tokenBData.data.data.tokenPrices.length - 1
           ].derivedNative;
         //need to return lastprice with 24 hours timestamps
       } else {
