d11df9a8144262ff35c8cfc3d5a262193579856a
Hammad Sanaullah <hammad.sanaullah@stixor.com>
2022-12-07 10:45:19 +0500

    week price data complete

--- routes/price.js
+++ routes/price.js
@@ -803,6 +803,282 @@ router.get("/getWeekPrices", async (req, res) => {
 
         return res.status(200).json({ data: generatedObject });
       }
+    } else if (
+      tokenAdaysnapshotsdata.data.data.tokenDaySnapshots.length != 0 &&
+      tokenBdaysnapshotsdata.data.data.tokenDaySnapshots.length != 0
+    ) {
+      //if tokenhoursnapshots exist but the priceNative is 0
+      if (
+        tokenAdaysnapshotsdata.data.data.tokenDaySnapshots[
+          tokenAdaysnapshotsdata.data.data.tokenDaySnapshots.length - 1
+        ].priceNative == 0 ||
+        tokenAdaysnapshotsdata.data.data.tokenDaySnapshots[
+          tokenAdaysnapshotsdata.data.data.tokenDaySnapshots.length - 1
+        ].priceNative == 0
+      ) {
+        //return create a pair with WBNB
+        return res
+          .status(500)
+          .json({ message: "No pair created with WBNB!!!" });
+      } else {
+        var priceAArray = [];
+        var timeStampsAArray = [];
+
+        for (
+          var i = 0;
+          i < tokenAdaysnapshotsdata.data.data.tokenDaySnapshots.length;
+          i++
+        ) {
+          priceAArray.push(
+            tokenAdaysnapshotsdata.data.data.tokenDaySnapshots[i].priceNative
+          );
+          timeStampsAArray.push(
+            tokenAdaysnapshotsdata.data.data.tokenDaySnapshots[i].date
+          );
+        }
+
+        var priceBArray = [];
+        var timeStampsBArray = [];
+        for (
+          var i = 0;
+          i < tokenBdaysnapshotsdata.data.data.tokenDaySnapshots.length;
+          i++
+        ) {
+          priceBArray.push(
+            tokenBdaysnapshotsdata.data.data.tokenDaySnapshots[i].priceNative
+          );
+          timeStampsBArray.push(
+            tokenBdaysnapshotsdata.data.data.tokenDaySnapshots[i].date
+          );
+        }
+
+        var timestamps = await generateTimeStampsForWeek(dayStartTime);
+
+        var generatedObject = [];
+        var count = 0;
+        var countB = 0;
+
+        for (var i = 0; i < timestamps.length; i++) {
+          time = timestamps[i];
+          if (timestamps[i] <= timeStampsAArray[count]) {
+            if (timestamps[i] <= timeStampsBArray[countB]) {
+              generatedObject.push({
+                info: {
+                  time,
+                  derivedBNB: priceBArray[countB] / priceAArray[count],
+                },
+              });
+            } else if (priceBArray[countB] == undefined) {
+              generatedObject.push({
+                info: {
+                  time,
+                  derivedBNB: priceBArray[countB - 1] / priceAArray[count],
+                },
+              });
+            } else {
+              countB = countB + 1;
+              if (priceBArray[countB] == undefined) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB - 1] / priceAArray[count],
+                  },
+                });
+              } else {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB] / priceAArray[count],
+                  },
+                });
+              }
+            }
+          } else if (timeStampsAArray[count] == undefined) {
+            if (timestamps[i] <= timeStampsBArray[countB]) {
+              generatedObject.push({
+                info: {
+                  time,
+                  derivedBNB: priceBArray[countB] / priceAArray[count - 1],
+                },
+              });
+            } else if (priceBArray[countB] == undefined) {
+              generatedObject.push({
+                info: {
+                  time,
+                  derivedBNB: priceBArray[countB - 1] / priceAArray[count - 1],
+                },
+              });
+            } else {
+              countB = countB + 1;
+              if (priceBArray[countB] == undefined) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB:
+                      priceBArray[countB - 1] / priceAArray[count - 1],
+                  },
+                });
+              } else {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB] / priceAArray[count - 1],
+                  },
+                });
+              }
+            }
+          } else {
+            count = count + 1;
+            if (priceAArray[count] == undefined) {
+              if (timestamps[i] <= timeStampsBArray[countB]) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB] / priceAArray[count - 1],
+                  },
+                });
+              } else if (priceBArray[countB] == undefined) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB:
+                      priceBArray[countB - 1] / priceAArray[count - 1],
+                  },
+                });
+              } else {
+                countB = countB + 1;
+                if (priceBArray[countB] == undefined) {
+                  generatedObject.push({
+                    info: {
+                      time,
+                      derivedBNB:
+                        priceBArray[countB - 1] / priceAArray[count - 1],
+                    },
+                  });
+                } else {
+                  generatedObject.push({
+                    info: {
+                      time,
+                      derivedBNB: priceBArray[countB] / priceAArray[count - 1],
+                    },
+                  });
+                }
+              }
+            } else {
+              if (timestamps[i] <= timeStampsBArray[countB]) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB] / priceAArray[count],
+                  },
+                });
+              } else if (priceBArray[countB] == undefined) {
+                generatedObject.push({
+                  info: {
+                    time,
+                    derivedBNB: priceBArray[countB - 1] / priceAArray[count],
+                  },
+                });
+              } else {
+                countB = countB + 1;
+                if (priceBArray[countB] == undefined) {
+                  generatedObject.push({
+                    info: {
+                      time,
+                      derivedBNB: priceBArray[countB - 1] / priceAArray[count],
+                    },
+                  });
+                } else {
+                  generatedObject.push({
+                    info: {
+                      time,
+                      derivedBNB: priceBArray[countB] / priceAArray[count],
+                    },
+                  });
+                }
+              }
+            }
+          }
+        }
+
+        return res.status(200).json({ data: generatedObject });
+      }
+    } else {
+      //else check tokenData for last price
+      let tokenAData = await axios({
+        url: "https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet",
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+          "Accept-Encoding": "utf-8",
+        },
+        data: {
+          query: `{
+            tokenPrices(where: {token_: {symbol: "${req.body.symbolA}"}}) {
+              id
+              lastUsdPrice
+              derivedNative
+            }
+          }
+            `,
+        },
+      });
+
+      let tokenBData = await axios({
+        url: "https://api.thegraph.com/subgraphs/name/hammadsanaullah/pancakeswapmumbaitestnet",
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+          "Accept-Encoding": "utf-8",
+        },
+        data: {
+          query: `{
+            tokenPrices(where: {token_: {symbol: "${req.body.symbolB}"}}) {
+              id
+              lastUsdPrice
+              derivedNative
+            }
+          }
+            `,
+        },
+      });
+      if (
+        tokenAData.data.data.tokenPrices.length != 0 ||
+        tokenBData.data.data.tokenPrices.length != 0
+      ) {
+        // for(var i = 0; i < tokenData.data.data.tokenPrices.length; i++ ) {
+
+        // }
+        // console.log(tokenData.data.data.tokenPrices)
+        var lastPriceA =
+          tokenAData.data.data.tokenPrices[
+            tokenAData.data.data.tokenPrices.length - 1
+          ].derivedNative;
+
+        var lastPriceB =
+          tokenBData.data.data.tokenPrices[
+            tokenBData.data.data.tokenPrices.length - 1
+          ].derivedNative;
+        //need to return lastprice with 24 hours timestamps
+      } else {
+        return res
+          .status(500)
+          .json({ message: "No pair created with WBNB!!!" });
+      }
+      var timestamps = await generateTimeStampsForWeek(dayStartTime);
+
+
+      var generatedObject = [];
+      var count = 0;
+      var countB = 0;
+
+      for (var i = 0; i < timestamps.length; i++) {
+        generatedObject.push({
+          info: { time, derivedBNB: lastPriceB / lastPriceA },
+        });
+      }
+
+      return res.status(200).json({ data: generatedObject });
     }
   } catch (error) {
     return res.status(400).json({ message: error.message });
